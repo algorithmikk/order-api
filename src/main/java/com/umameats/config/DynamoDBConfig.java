9e1fc6c84ca6ac5421f792a3f9a50a03d9f2fef8
jay <jawadarifpro@gmail.com>
2024-11-13 12:10:19 -0500

    update configs

--- src/main/java/com/umameats/config/DynamoDBConfig.java
+++ src/main/java/com/umameats/config/DynamoDBConfig.java
@@ -4,34 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 @Configuration
 public class DynamoDBConfig {
-    @Value("${aws.access.key.id}")
-    private String accessKeyId;
-
-    @Value("${aws.secret.access.key}")
-    private String secretKey;
-
+    
     @Value("${aws.region}")
     private String region;
-
+    
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(buildDynamoDBClient());
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+            .withRegion(region)
+            // No credentials needed - will use ECS Task Role
+            .build();
     }
 
-    private AmazonDynamoDB buildDynamoDBClient() {
-        return AmazonDynamoDBClientBuilder
-                .standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(
-                        new BasicAWSCredentials(accessKeyId, secretKey)))
-                .build();
+    @Bean
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+        return new DynamoDBMapper(amazonDynamoDB);
     }
 }
