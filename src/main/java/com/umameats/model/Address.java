f3ac087e449c0339357724b4fa9e66a231e10d8e
jay <jawadarifpro@gmail.com>
2025-02-22 18:21:21 -0500

    add missin gannotation

--- src/main/java/com/umameats/model/Address.java
+++ src/main/java/com/umameats/model/Address.java
@@ -1,12 +1,14 @@
 package com.umameats.model;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel.DynamoDBAttributeType;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
 
 import lombok.Data;
 
 @Data
+@DynamoDBDocument  // Add this annotation
  public class Address {
     @DynamoDBAttribute
     @DynamoDBTyped(DynamoDBAttributeType.S)
