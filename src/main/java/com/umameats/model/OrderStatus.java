548b7e952e954fcdbd2e3597d13f43329b070027
jay <jawadarifpro@gmail.com>
2025-10-25 20:38:00 +0200

    Add DRIVER_EN_ROUTE_TO_STORE status to OrderStatus enum

--- src/main/java/com/umameats/model/OrderStatus.java
+++ src/main/java/com/umameats/model/OrderStatus.java
@@ -7,7 +7,8 @@ public enum OrderStatus {
     CONFIRMED,
     PREPARING,
     READY_FOR_PICKUP,
-    OUT_FOR_DELIVERY,
+    DRIVER_EN_ROUTE_TO_STORE,  // Driver accepted and heading to restaurant
+    OUT_FOR_DELIVERY,           // Driver picked up food and heading to customer
     DELIVERED,
     CANCELLED
 }
